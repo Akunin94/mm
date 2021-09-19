5cbdd0e094e129f8413a1f5f6cf46158a9d2bd96
Davron <akunin94@gmail.com>
2021-09-19 16:43:47 +0500

    category page ended

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
 import $ from 'jquery'
 import 'slick-carousel'
+import noUiSlider from 'nouislider'
+import 'jquery.formstyler-modern'
 
 $(function(){
 	if ($('.main__slider--js').length) {
@@ -8,4 +10,61 @@ $(function(){
 			autoplay: true
 		})
 	}
+	if ($('.viewed__slider').length) {
+		$('.viewed__slider').slick({
+			rows: false,
+			autoplay: false
+		})
+	}
+
+	if ( $('.select-wrap select').length ) {
+		$('.select-wrap select').styler({
+			locale: 'ru'
+		})
+	}
+	
+	$('.filter-slider').each(function(){
+		let $this = $(this),
+			slider = $this.get(0),
+			inputMin = $this.next().find('.min'),
+			inputMax = $this.next().find('.max'),
+			min = $this.data('min'),
+			max = $this.data('max'),
+			step = $this.data('step');
+
+		noUiSlider.create(slider, {
+			start: [min, max/2],
+			connect: true,
+			step,
+			range: {
+				'min': min,
+				'max': max
+			}
+		});
+
+		slider.noUiSlider.on('update', function(values){
+			inputMin.val(+values[0]);
+			inputMax.val(+values[1])
+		})
+	});
+});
+
+$(document).on('click', '.category__filters .title', function(){
+	$(this).toggleClass('active');
+	$(this).next().slideToggle();
+});
+$(document).on('click', '.filter-button', function(){
+	$(this).toggleClass('active');
+});
+$(document).on('click', '.filter-color-button', function(){
+	$(this).toggleClass('active');
+});
+$(document).on('click', '.langs > span:not(.active)', function(){
+	let ind = $(this).index();
+
+	$(this).siblings().removeClass('active');
+	$(this).addClass('active');
+
+	$(this).parent().next().find('.langs-body--item').removeClass('active');
+	$(this).parent().next().find('.langs-body--item').eq(ind).addClass('active');
 });
